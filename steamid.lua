519c96752c80088822434f9fe9549afe9fa7445b
DESALIN <oxothukvip254@gmail.com>
2025-01-02 06:11:57 +0200

    Выводит в консоли ваш steamid

---
+++ steamid.lua
@@ -0,0 +1,24 @@
+-- Получение уникального идентификатора игрока (например, Steam ID)
+local function get_unique_id()
+    local steam_id = entity.get_steam64(entity.get_local_player())
+    if steam_id then
+        return steam_id
+    else
+        return "Не удалось получить Steam ID"
+    end
+end
+
+-- Флаг для предотвращения повторного вывода
+local displayed = false
+
+-- Обработчик для вывода ID
+local function display_id()
+    if not displayed then
+        local unique_id = get_unique_id()
+        client.log("Уникальный идентификатор (Steam ID): " .. unique_id)
+        displayed = true
+    end
+end
+
+-- Вывод идентификатора при запуске
+client.set_event_callback("paint", display_id)
